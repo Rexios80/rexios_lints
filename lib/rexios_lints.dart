445f781ccb96ca92a812440a8cfba87b2348e532
Rexios <rexios@rexios.dev>
2024-09-12 16:01:17 -0400

    Refactor folder structure

--- lib/rexios_lints.dart
+++ lib/rexios_lints.dart
@@ -1,7 +1,7 @@
 import 'package:custom_lint_builder/custom_lint_builder.dart';
-import 'package:rexios_lints/lints/do_not_use_raw_paths.dart';
-import 'package:rexios_lints/lints/prefer_async_await.dart';
-import 'package:rexios_lints/lints/prefer_timestamps.dart';
+import 'package:rexios_lints/custom_lint/do_not_use_raw_paths.dart';
+import 'package:rexios_lints/custom_lint/prefer_async_await.dart';
+import 'package:rexios_lints/custom_lint/prefer_timestamps.dart';
 
 /// Create the linter plugin
 PluginBase createPlugin() => _RexiosLinter();
