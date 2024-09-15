c055fdd5ec7844b3eaf4a79ec95b545e98fa9ab2
Rexios <rexios@rexios.dev>
2024-09-15 16:07:10 -0400

    Add fix for do_not_use_raw_paths

--- lib/custom_lint/do_not_use_raw_paths.dart
+++ lib/custom_lint/do_not_use_raw_paths.dart
@@ -3,6 +3,8 @@ import 'package:analyzer/error/error.dart' hide LintCode;
 import 'package:analyzer/error/listener.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
+final _pathSeparatorRegex = RegExp(r'[\\/]');
+
 /// Do not use raw paths
 class DoNotUseRawPaths extends DartLintRule {
   static const _code = LintCode(
@@ -37,9 +39,44 @@ class DoNotUseRawPaths extends DartLintRule {
       if (pathArgument is! StringLiteral) return;
 
       final path = pathArgument.stringValue;
-      if (path == null || !{'/', '\\'}.any(path.contains)) return;
+      if (path == null || !_pathSeparatorRegex.hasMatch(path)) return;
 
-      reporter.atNode(node, _code);
+      reporter.atNode(pathArgument, _code, data: path);
+    });
+  }
+
+  @override
+  List<Fix> getFixes() => [_UsePathJoinFix()];
+}
+
+class _UsePathJoinFix extends DartFix {
+  @override
+  void run(
+    CustomLintResolver resolver,
+    ChangeReporter reporter,
+    CustomLintContext context,
+    AnalysisError analysisError,
+    List<AnalysisError> others,
+  ) {
+    context.registry.addInstanceCreationExpression((node) {
+      if (!analysisError.sourceRange.intersects(node.sourceRange)) return;
+
+      final builder = reporter.createChangeBuilder(
+        message: 'Use path.join(...)',
+        // TODO: Is there a standard priority for lint fixes
+        priority: 999,
+      );
+
+      final path = analysisError.data as String;
+      final segments = path.split(_pathSeparatorRegex);
+      final segmentsString = segments.map((e) => "'$e'").join(', ');
+
+      builder.addDartFileEdit((builder) {
+        builder.addSimpleReplacement(
+          analysisError.sourceRange,
+          'path.join($segmentsString)',
+        );
+      });
     });
   }
 }
