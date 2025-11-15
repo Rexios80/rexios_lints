dfdbd1dd6cf7116d71d8278af2423c33620bc3a0
Rexios <rexios@rexios.dev>
2025-11-15 17:23:37 -0500

    timestamps test

---
+++ test/analyzer_plugin/prefer_timestamps_test.dart
@@ -0,0 +1,32 @@
+import 'package:analyzer_testing/analysis_rule/analysis_rule.dart';
+import 'package:rexios_lints/analyzer_plugin/prefer_timestamps.dart';
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+import 'package:analyzer/src/lint/registry.dart';
+
+@reflectiveTest
+class PreferTimestampsTest extends AnalysisRuleTest {
+  @override
+  String get analysisRule => PreferTimestamps.code.name;
+
+  @override
+  void setUp() {
+    Registry.ruleRegistry.registerLintRule(PreferTimestamps());
+    super.setUp();
+  }
+
+  void test_invalid() async {
+    await assertDiagnostics('final _ = DateTime.now();', [lint(10, 14)]);
+  }
+
+  // TODO: Figure out why this is not working
+  @skippedTest
+  void test_valid() async {
+    await assertNoDiagnostics('final _ = DateTime.timestamp();');
+  }
+}
+
+void main() {
+  defineReflectiveSuite(() {
+    defineReflectiveTests(PreferTimestampsTest);
+  });
+}
